d5eadff119697b8c8f051a0f2c9705a4c89a11bf
simon <simon.hall@assurity.co.nz>
2021-07-28 11:14:25 +1200

    Update cricketCalc.js
    subtracted last character from the substring in backspace function

--- web/js/cricketCalc.js
+++ web/js/cricketCalc.js
@@ -19,7 +19,7 @@ function enter(key) {
 function backspace() {
     var previousBall = scorecard.charAt(scorecard.length - 1);
     score = score - previousBall;
-    scorecard = scorecard.substring(0, scorecard.length);
+    scorecard = scorecard.substring(0, scorecard.length - 1);
     document.getElementById('scorecard').innerHTML = scorecard;
     document.getElementById('score').innerHTML = score;
 }
